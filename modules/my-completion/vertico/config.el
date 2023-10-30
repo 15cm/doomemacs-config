c016786c5f7df29fbcd5309472c455349793076b
Sinkerine <git@15cm.net>
2023-10-30 02:27:03 -0700

    feat: update vertico minibuffer map

--- modules/my-completion/vertico/config.el
+++ modules/my-completion/vertico/config.el
@@ -13,7 +13,6 @@
 
 (defun my-vertico-search-file (&optional with-input)
   (interactive)
-  (require 'consult)
   (let* ((fname (file-name-nondirectory (buffer-file-name)))
          ;; Pass fname as an expression to avoid it being split by spaces.
          (consult-ripgrep-args `(,consult-ripgrep-args "-g" fname))
@@ -27,11 +26,20 @@
   (interactive)
   (my-vertico-search-file t))
 
-(map! 
+(map!
  :leader
- ;; :after 'consult
-    :desc "Search in dir" "sd" #'my-vertico-search-dir
-    :desc "Search in dir with things at point" "sD" #'my-vertico-search-dir-with-input
-    :desc "Search in file" "ss" #'my-vertico-search-file
-    :desc "Search in file with things at point" "sS" #'my-vertico-search-file-with-input
-    )
+ :after consult
+ :desc "Search in dir" "sd" #'my-vertico-search-dir
+ :desc "Search in dir with things at point" "sD" #'my-vertico-search-dir-with-input
+ :desc "Search in file" "ss" #'my-vertico-search-file
+ :desc "Search in file with things at point" "sS" #'my-vertico-search-file-with-input
+ )
+
+(map!
+ :after vertico
+ :map vertico-map
+ "<next>" #'vertico-scroll-up
+ "<prior>" #'vertico-scroll-down
+ "C-l" #'vertico-directory-up
+ "C-p" #'previous-history-element
+ "C-n" #'next-history-element)
