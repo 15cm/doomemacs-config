f00e73582ef9dd42e75b01d621b2064bf4ac3aab
Sinkerine <git@15cm.net>
2023-10-31 23:23:02 -0700

    feat: add treesit-auto package which doesn't work

---
+++ modules/my-tools/tree-sitter/config.el
@@ -0,0 +1,5 @@
+(use-package! treesit-auto
+  :init
+  (setq treesit-auto-install t)
+  :config
+  (global-treesit-auto-mode))
---
+++ modules/my-tools/tree-sitter/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; my-tools/tree-sitter/packages.el
+
+(package! treesit-auto)
