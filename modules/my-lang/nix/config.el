0d5cc68fdd085b8ade6867fcbf26696019eb7c77
Sinkerine <git@15cm.net>
2023-11-01 22:37:35 -0700

    feat: change the company backend of nix mode

--- modules/my-lang/nix/config.el
+++ modules/my-lang/nix/config.el
@@ -11,7 +11,7 @@
   :config
   (if (modulep! +lsp)
       (add-hook 'nix-mode-hook #'lsp! 'append)
-    (set-company-backend! 'nix-mode 'company-dabbrev))
+    (set-company-backend! 'nix-mode 'company-dabbrev-code))
   (map! :localleader
         :map nix-mode-map
         "=" #'nix-format-buffer
