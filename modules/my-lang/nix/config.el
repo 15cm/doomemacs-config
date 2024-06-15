73d136394f23ca40f5f221ba19f1c6b4697c9218
Sinkerine <git@15cm.net>
2024-06-15 15:59:18 -0700

    lsp: add lsp-ai config which doesn't work for now

--- modules/my-lang/nix/config.el
+++ modules/my-lang/nix/config.el
@@ -12,10 +12,33 @@
   (if (modulep! +lsp)
       (add-hook 'nix-mode-hook #'lsp! 'append)
     (set-company-backend! 'nix-mode 'company-dabbrev-code))
+
   (map! :localleader
         :map nix-mode-map
         "=" #'nix-format-buffer
         "'" #'nix-repl-show)
+
+  ;; Not working.
+  ;; (when (modulep! +lsp-ai)
+  ;;     (after! lsp-mode
+  ;;       (lsp-register-client
+  ;;        (make-lsp-client :new-connection (lsp-stdio-connection (lambda () "lsp-ai"))
+  ;;                         :major-modes '(nix-mode)
+  ;;                         :initialization-options (lambda () (append
+  ;;                                                             `(:memory (:file_store (:crawl :json-false)))
+  ;;                                                             `(:models (:model1 (:type "ollama" :model "deepseek-coder")))
+  ;;                                                             `(:completion (:model "model1" :parameters (:max_context 2000 (:options (:num_predict 32)))))
+  ;;                                                             ))
+  ;;                         :initialized-fn (lambda (workspace)
+  ;;                                           (with-lsp-workspace workspace
+  ;;                                             (lsp--set-configuration
+  ;;                                              (lsp-configuration-section "nil"))))
+  ;;                         :synchronize-sections '("nil")
+  ;;                         :server-id 'lsp-ai))
+  ;;       )
+  ;;     (add-hook 'nix-mode-hook #'lsp-deferred)
+  ;;     (add-hook 'nix-mode-hook (lambda () (lsp-completion-mode 1)))
+  ;;     )
   )
 
 (use-package! nix-repl
